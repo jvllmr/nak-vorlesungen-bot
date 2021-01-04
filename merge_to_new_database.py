2240b2e181218449272c6e514979cfef5c948449
kreyoo <zunder325@gmail.com>
2021-01-04 13:28:35 +0100

    fix

--- merge_to_new_database.py
+++ merge_to_new_database.py
@@ -2,7 +2,7 @@ import sqlite3, os
 
 newdata = sqlite3.connect("newdatabase.db")
 olddata = sqlite3.connect("database.db")
-
+newdata.execute("create table meetings (server integer, channel integer, assignment_name text,id text,dozent text, year integer, month integer, day integer, time integer, link text, kennwort text)")
 for tobereplaced in olddata.execute("select * from meetings").fetchall():
     newline= list()
     for i in tobereplaced:
