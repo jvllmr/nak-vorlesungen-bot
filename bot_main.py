1b8eed2fb8493a695e1996d8befce6a2b987daf0
kreyoo <zunder325@gmail.com>
2021-04-10 12:00:29 +0200

    Fetch iCalender from link

--- bot_main.py
+++ bot_main.py
@@ -1,4 +1,4 @@
-import discord, sqlite3, asyncio, os, re, datetime
+import discord, sqlite3, asyncio, os, re, datetime, requests, codecs
 from icalendar import Calendar
 
 
@@ -24,6 +24,7 @@ class botclient(discord.Client):
 
         # create the background task and run it in the background
         self.assignment_check = self.loop.create_task(self.check_for_next_assignment())
+        self.assignment_refresher = self.loop.create_task(self.refresh_assignments())
         self.sql = sqlite3.connect("database.db")
         self.prefix = "_"
         self.waitforreaction = dict()
@@ -50,74 +51,79 @@ class botclient(discord.Client):
         if message.author == self.user:
             return
         
-        elif message.content == self.prefix+"upload":
+        elif message.content.split(" ")[0] == self.prefix+"upload":
             if not await self.check_authentication(message):
                 return
-            if not message.attachments:
+            elif len(message.content.split(" ")) < 2:
                 await message.add_reaction("\U0000274C")
-                await message.channel.send("\U0000274C ***[FAILED]*** Bitte hänge eine Datei an deine Nachricht an")
+                await message.channel.send("\U0000274C ***[FAILED]*** Bitte gebe einen Link an")
                 return
 
             else:
+                http_link = message.content.split(" ")[1]
+                try:
+                    r = requests.get(http_link)
+                except Exception as err:
+                    await message.add_reaction("\U0000274C")
+                    await message.channel.send("\U0000274C ***[FAILED]*** Gegebener Link brachte einen Fehler: "+str(err))
+                    return
+                d = r.headers['content-disposition']
+                filename = re.findall("filename=(.+)", d)[0]
+                mastermessage = "`\U0001F504 [RUNNING] Lese "+ filename + "`\n"
+                currentmessage = await message.channel.send("\U0001F504 ***[RUNNING]*** Lese "+ filename)
+                
+                if not ".ics" in filename:
+                    await message.add_reaction("\U0000274C")
+                    await currentmessage.edit(content="\U0000274C ***[FAILED]*** "+ filename + " ist keine iCalender-Datei")
+                    return
+                
+                icscal = Calendar.from_ical(codecs.encode(codecs.decode(r.content,encoding="ANSI"),encoding="utf-8"))
+                x=0
+                for component in icscal.walk():
 
-                for f in message.attachments:
-                    mastermessage = "`\U0001F504 [RUNNING] Lese "+ f.filename + "`\n"
-                    currentmessage = await message.channel.send("\U0001F504 ***[RUNNING]*** Lese "+ f.filename)
+                    if component.name == "VEVENT":
+                        
+                        await asyncio.sleep(0.5)
+                        await currentmessage.edit(content=mastermessage+ "\U0001F504 ***[RUNNING]*** Füge Termin "+ component.get("summary")+ " hinzu")
 
-                    if not ".ics" in f.filename:
-                        await message.add_reaction("\U0000274C")
-                        await currentmessage.edit(content="\U0000274C ***[FAILED]*** "+f.filename + " ist keine iCalender-Datei")
-                        return
-
-                    await f.save(f.filename)
-                    localfile = open(f.filename,"rb")
-                    icscal = Calendar.from_ical(localfile.read())
-                    x=0
-                    for component in icscal.walk():
-
-                        if component.name == "VEVENT":
+                        for i in component.get("summary").split(","):
                             
-                            await asyncio.sleep(0.5)
-                            await currentmessage.edit(content=mastermessage+ "\U0001F504 ***[RUNNING]*** Füge Termin "+ component.get("summary")+ " hinzu")
+                            if regex:=re.search(r"[A-Z]\d\d\d",i):
+                                assignment_name = i.lstrip("V")
+                                module_id = regex.group()
+                                break
 
-                            for i in component.get("summary").split(","):
-                                
-                                if regex:=re.search(r"[A-Z]\d\d\d",i):
-                                    assignment_name = i.lstrip("V")
-                                    module_id = regex.group()
-                                    break
+                            elif "Zenturienbetreuung" in i:
+                                assignment_name = i
+                                module_id = "Z"
+                                break
+                        dozent=component.get("summary").split(",")[2]
+                        zenturie = component.get("summary").split(",")[0]
 
-                                elif "Zenturienbetreuung" in i:
-                                    assignment_name = i
-                                    module_id = "Z"
-                                    break
-                            dozent=component.get("summary").split(",")[2]
-                            zenturie = component.get("summary").split(",")[0]
-
-                            if not module_id:  
-                                await message.add_reaction("\U0000274C")
-                                await currentmessage.edit(content="\U0000274C ***[FAILED]*** Konnte in "+f.filename+" nicht die Modul-ID vom Termin "+component.get("summary").encode()+" finden")
-                                return
-                            
-                            datetime = str(component.get("DTSTART").dt).split(" ")
-                            year = int(datetime[0].split("-")[0])
-                            month = int(datetime[0].split("-")[1])
-                            day = int(datetime[0].split("-")[2])
-                            time = int(datetime[1].split(":")[0] + datetime[1].split(":")[1])
-                            data = (message.guild.id,message.channel.id,assignment_name,module_id,dozent,year,month,day,time,"NULL","NULL",zenturie)
-                            if self.sql.execute("select * from meetings where server=? and channel=? and assignment_name=? and dozent=? and year=? and month=? and day=? and time=?",(message.guild.id,message.channel.id,assignment_name,dozent,year,month,day, time)).fetchone():
-                                print(locationbracket+timebracket()+"Meeting "+component.get("summary")+" existiert bereits")
-                            else:
-                                print(locationbracket+timebracket()+"Meeting "+component.get("summary")+" erstellt")
-                                self.sql.execute("insert into meetings values (?,?,?,?,?,?,?,?,?,?,?,?)", data)
-                                self.sql.commit()
-                                x = x+1
-                    
-                    localfile.close()
-                    os.remove(f.filename)
-                    
-                    await message.add_reaction("\U00002705")
-                    await currentmessage.edit(content="\U00002705 ***[DONE]*** Erfolgreich "+str(x)+" neue Termine aus der Datei "+f.filename + " migriert")
+                        if not module_id:  
+                            await message.add_reaction("\U0000274C")
+                            await currentmessage.edit(content="\U0000274C ***[FAILED]*** Konnte in "+filename+" nicht die Modul-ID vom Termin "+component.get("summary").encode()+" finden")
+                            return
+                        
+                        datetime = str(component.get("DTSTART").dt).split(" ")
+                        year = int(datetime[0].split("-")[0])
+                        month = int(datetime[0].split("-")[1])
+                        day = int(datetime[0].split("-")[2])
+                        time = int(datetime[1].split(":")[0] + datetime[1].split(":")[1])
+                        data = (message.guild.id,message.channel.id,assignment_name,module_id,dozent,year,month,day,time,"NULL","NULL",zenturie,http_link)
+                        if self.sql.execute("select * from meetings where server=? and channel=? and assignment_name=? and dozent=? and year=? and month=? and day=? and time=?",(message.guild.id,message.channel.id,assignment_name,dozent,year,month,day, time)).fetchone():
+                            print(locationbracket+timebracket()+"Meeting "+component.get("summary")+" existiert bereits")
+                        else:
+                            print(locationbracket+timebracket()+"Meeting "+component.get("summary")+" erstellt")
+                            self.sql.execute("insert into meetings values (?,?,?,?,?,?,?,?,?,?,?,?,?)", data)
+                            self.sql.commit()
+                            x = x+1
+                
+                
+                
+                
+                await message.add_reaction("\U00002705")
+                await currentmessage.edit(content="\U00002705 ***[DONE]*** Erfolgreich "+str(x)+" neue Termine aus der Datei "+filename + " migriert")
 
         elif re.search("^["+self.prefix+"][l][i][n][k]", message.content):
             if not await self.check_authentication(message):
@@ -327,6 +333,17 @@ class botclient(discord.Client):
                 await asyncio.sleep(60)
         except Exception as err:
             self.assignment_check = self.loop.create_task(self.check_for_next_assignment())
+            raise err
+
+    async def refresh_assignments(self):
+        await self.wait_until_ready()
+        try:
+            await asyncio.sleep(60)
+            while not self.is_closed():
+                if datetime.datetime.strftime() == "" and datetime.datetime.strftime() == "":
+                    pass
+        except Exception as err:
+            self.refresh_assignments = self.loop.create_task(self.refresh_assignments())
             raise err
 
 try:
