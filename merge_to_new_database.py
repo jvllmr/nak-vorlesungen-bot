556771fbf0aa2f4c955e6ca05c6f0d81d15e588a
kreyoo <zunder325@gmail.com>
2021-04-22 17:42:21 +0200

    For prod upgrade to new mechanisms

--- merge_to_new_database.py
+++ merge_to_new_database.py
@@ -2,13 +2,10 @@ import sqlite3, os
 
 newdata = sqlite3.connect("newdatabase.db")
 olddata = sqlite3.connect("database.db")
-newdata.execute("create table meetings (server integer, channel integer, assignment_name text,id text,dozent text, year integer, month integer, day integer, time integer, link text, kennwort text, zenturie text)")
-for tobereplaced in olddata.execute("select * from meetings").fetchall():
-    newline= list()
-    for i in tobereplaced:
-        newline.append(i)
-    newline.append("A20b")
-    newdata.execute("insert into meetings values (?,?,?,?,?,?,?,?,?,?,?,?)",tuple(newline))
+newdata.execute("create table meetings (server integer, channel integer, assignment_name text,id text,dozent text, year integer, month integer, day integer, time integer, link text, kennwort text, zenturie text, fetch_link text)")
+for line in olddata.execute("select * from meetings").fetchall():
+    newdata.execute("insert into meetings values (?,?,?,?,?,?,?,?,?,?,?,?)",line)
+newdata.execute("create table bindings (server ingeger, channel integer, zenturie text)")
 newdata.commit()
 olddata.close()
 newdata.close()
