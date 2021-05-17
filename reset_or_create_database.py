65b50fa0ba5e1d862ea070a510f330427c521ec1
kreyoo <zunder325@gmail.com>
2021-05-17 23:42:25 +0200

    moodle table in db creation file

--- reset_or_create_database.py
+++ reset_or_create_database.py
@@ -9,10 +9,12 @@ sqlcon = sqlite3.connect("database.db")
 try:
     sqlcon.execute("drop table meetings")
     sqlcon.execute("drop table bindings")
+    sqlcon.execute("drop table moodle")
 except sqlite3.OperationalError:
     print("Tabelle meetings existiert noch nicht und wird erstellt")
 sqlcon.execute("create table meetings (server integer, channel integer, assignment_name text,id text,dozent text, year integer, month integer, day integer, time integer, link text, kennwort text, zenturie text, fetch_link text)")
-sqlcon.execute("create table bindings (server ingeger, channel integer, zenturie text)")
+sqlcon.execute("create table bindings (server integer, channel integer, zenturie text)")
+sqlcon.execute("create table moodle (channel integer,id text, moodle_link text)")
 sqlcon.commit()
 sqlcon.close()
 print("Datenbank erfolgreich eingerichtet / zurückgesetzt")
