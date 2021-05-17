dec2a2bbfdac869dbd0f87ae9673b8c750727275
kreyoo <zunder325@gmail.com>
2021-05-17 23:41:00 +0200

    fix moodle command not being detected

--- bot_main.py
+++ bot_main.py
@@ -177,7 +177,7 @@ class botclient(discord.Client):
                     traceback.print_tb(err.__traceback__)
                     return
 
-        elif re.search("^["+self.prefix+"][m][o][o][d][l][e]", message.content):
+        elif message.content.split(" ")[0] == self.prefix+"moodle":
             try:
                 modul = message.split(" ")[1]
             except IndexError:
